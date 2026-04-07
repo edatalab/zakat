4f89724cf78c2a7f705785fae28367e9ad251cab
edatalab <syed_q@yahoo.com>
2026-04-06 23:11:37 -0400

    deployment config updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./zakat/", // <<--- important
+    base: "/zakat/", // <<--- important
   server: {
     host: "::",
     port: 8080,
